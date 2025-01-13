22f7b16ae76de3f070207bb0848c3719ce0eed80
Karan Popat <krn.ppt@gmail.com>
2025-01-13 18:01:30 +0100

    13012025_1230 - Add a snake as a dot.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/hajimehoshi/ebiten/v2/vector"
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -9,15 +11,33 @@ import (
 const (
 	screenWidth = 640
 	screenHeigh = 480
+	gridSize    = 16
 )
 
-type Game struct{}
+type Point struct {
+	x, y int
+}
+
+type Game struct {
+	snake []Point
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	for _, p := range g.snake {
+		vector.DrawFilledRect(
+			screen,
+			float32(p.x*gridSize),
+			float32(p.y*gridSize),
+			gridSize,
+			gridSize,
+			color.White,
+			true,
+		)
+	}
 
 }
 
@@ -28,7 +48,12 @@ func (g *Game) Layout(
 }
 
 func main() {
-	g := &Game{}
+	g := &Game{
+		snake: []Point{{
+			x: screenWidth / gridSize / 2,
+			y: screenWidth / gridSize / 2,
+		}},
+	}
 
 	ebiten.SetWindowSize(screenWidth, screenHeigh)
 	ebiten.SetWindowTitle("Snake Game")
